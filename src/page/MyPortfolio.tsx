314123ef76da958f53a90b35d5d5811a6b8c1488
Nizam Yuseri <nizamhiga@gmail.com>
2025-12-22 15:43:51 +0800

    improve personal project desc

--- src/page/MyPortfolio.tsx
+++ src/page/MyPortfolio.tsx
@@ -18,7 +18,7 @@ export function MyPortfolio() {
                     <Box className="portfolio-item">
                       <img src={resumeThumb} alt="Project" />
                       <h3>AI powered Resume Analyzer</h3>
-                      <p>ReactJS, MUI, Tailwind, C#, MSSQL, Docker</p>
+                      <p>A web application that automatically scans and analyzes candidate resumes against a job description (JD), helping hiring managers quickly shortlist suitable candidates and allowing job seekers to optimize their resumes for better matching. The system extracts key skills, experience, and qualifications from resumes and provides a match score and detailed insights, reducing manual screening time and improving hiring efficiency.</p>
 
                           <Box className="portfolio-tags">
                           {visibleTags.map((tag) => (
